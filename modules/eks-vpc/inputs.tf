e6ee3f4492f7a2b510958041d2026b214ecc7783
Tarlan Huseynov <thuseynov@vmi2066266.contaboserver.net>
2024-08-13 16:41:45 +0200

    init

---
+++ modules/eks-vpc/inputs.tf
@@ -0,0 +1,4 @@
+variable "cluster_name" {}
+variable "eks_vpc_cidr" {}
+variable "az_count" {}
+variable "env" {}
---
+++ modules/eks-vpc/main.tf
@@ -0,0 +1,38 @@
+data "aws_availability_zones" "available" {}
+
+locals {
+  azs              = slice(data.aws_availability_zones.available.names, 0, var.az_count)
+  public_subnets   = slice([for i in range(2, 16, 2) : cidrsubnet(var.eks_vpc_cidr, 8, i)], 0, var.az_count)
+  private_subnets  = slice([for i in range(1, 16, 2) : cidrsubnet(var.eks_vpc_cidr, 8, i)], 0, var.az_count)
+  database_subnets = slice([for i in range(1, 16, 2) : cidrsubnet(var.eks_vpc_cidr, 8, i)], var.az_count, 2 * var.az_count)
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.12.1"
+
+  name                         = var.cluster_name
+  cidr                         = var.eks_vpc_cidr
+  azs                          = local.azs
+  public_subnets               = local.public_subnets
+  private_subnets              = local.private_subnets
+  database_subnets             = local.database_subnets
+  create_database_subnet_group = true
+  create_vpc                   = true
+  enable_nat_gateway           = true
+  single_nat_gateway           = var.env != "prod" ? true : false
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                    = 1
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+    "karpenter.sh/discovery"                    = var.cluster_name # TODO: to be determined during karpenter implementation
+    Accessibility                               = "public"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"           = 1
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+    "karpenter.sh/discovery"                    = var.cluster_name # TODO: to be determined during karpenter implementation
+    Accessibility                               = "private"
+  }
+}
