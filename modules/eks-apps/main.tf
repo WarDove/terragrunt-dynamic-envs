7495ba48c147f7d8ca1e5b84a3f766aa07eeda91
Tarlan Huseynov <thuseynov@vmi2066266.contaboserver.net>
2024-08-14 21:33:50 +0200

    init

--- modules/eks-apps/main.tf
+++ modules/eks-apps/main.tf
@@ -3,3 +3,13 @@ data "aws_availability_zones" "available" {}
 locals {
   azs = slice(data.aws_availability_zones.available.names, 0, var.az_count)
 }
+
+
+/*
+
+TODO: implement AWS ALB Controller
+TODO: implement external secrets
+TODO: implement external dns
+TODO: Implement imitation of dynamic Enviironments
+
+*/
