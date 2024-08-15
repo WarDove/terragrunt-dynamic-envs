db7df5cafa3145b01027c01b301d07e17c9227fc
Tarlan Huseynov <thuseynov@vmi2066266.contaboserver.net>
2024-08-15 18:42:44 +0200

    init

--- modules/eks-apps/main.tf
+++ modules/eks-apps/main.tf
@@ -1,7 +1,7 @@
 
 /*
 
-TODO: implement external secrets
+TODO: Where to put awsecrets and cluster secret store and it's binding
 TODO: implement external dns
 TODO: Implement imitation of dynamic Enviironments
 
