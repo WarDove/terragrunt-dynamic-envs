79039ae6c7cffd9a83d5a17860d946582f0e2ca3
Tarlan Huseynov <thuseynov@vmi2066266.contaboserver.net>
2024-08-16 12:16:59 +0200

    init

---
+++ modules/eks-crds/external-secrets.tf
@@ -0,0 +1,73 @@
+resource "kubernetes_manifest" "cluster_secret_store_aws_secrets" {
+  count = var.enable_es ? 1 : 0
+
+  manifest = {
+    apiVersion = "external-secrets.io/v1beta1"
+    kind       = "ClusterSecretStore"
+    metadata = {
+      name = "cluster-secret-store-aws-secrets"
+    }
+    spec = {
+      provider = {
+        aws = {
+          service = "SecretsManager"
+          region  = var.region
+          auth = {
+            jwt = {
+              serviceAccountRef = {
+                namespace : "external-secrets"
+                name = "cluster-secret-store"
+              }
+            }
+          }
+        }
+      }
+      conditions = [
+        {
+          namespaceSelector = {
+            matchLabels = {
+              "kubernetes.io/environment" = var.env # TODO: Change this to the correct label in future
+            }
+          }
+        }
+      ]
+    }
+  }
+}
+
+resource "kubernetes_manifest" "cluster_secret_store_aws_ssm" {
+  count = var.enable_es ? 1 : 0
+
+  manifest = {
+    apiVersion = "external-secrets.io/v1beta1"
+    kind       = "ClusterSecretStore"
+    metadata = {
+      name = "cluster-secret-store-aws-ssm"
+    }
+    spec = {
+      provider = {
+        aws = {
+          service = "ParameterStore"
+          region  = var.region
+          auth = {
+            jwt = {
+              serviceAccountRef = {
+                namespace : "external-secrets"
+                name = "cluster-secret-store"
+              }
+            }
+          }
+        }
+      }
+      conditions = [
+        {
+          namespaceSelector = {
+            matchLabels = {
+              "kubernetes.io/environment" = var.env
+            }
+          }
+        }
+      ]
+    }
+  }
+}
