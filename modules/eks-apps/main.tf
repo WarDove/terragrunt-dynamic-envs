50c453d54bae2bf61f749081db35990a531b5190
Tarlan Huseynov <thuseynov@vmi2066266.contaboserver.net>
2024-08-19 13:23:22 +0200

    launched albc

--- modules/eks-apps/main.tf
+++ modules/eks-apps/main.tf
@@ -1,8 +1,6 @@
 
 /*
-TODO: lockdown k8s api authentication
-TODO: implement external dns
-TODO: hosted zone and ACM management
 TODO: Where to put awsecrets
 TODO: Implement imitation of dynamic Enviironments
+TODO: Lockdown k8s api authentication
 */
