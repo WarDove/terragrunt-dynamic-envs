0cfc4e495cd2cf3be1172f468da919f606f4448e
Tarlan Huseynov <thuseynov@vmi2066266.contaboserver.net>
2024-08-21 18:35:46 +0200

    init

--- modules/eks-resources/main.tf
+++ modules/eks-resources/main.tf
@@ -1,6 +1,6 @@
 /*
-TODO: ArgoCD
 TODO: Where to put awsecrets
+TODO: ArgoCD notifications & webhook & gitops repo with helms
 TODO: Implement imitation of dynamic Enviironments
 TODO: Lockdown k8s api authentication
 */
