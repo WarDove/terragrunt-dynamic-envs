48b2764c0e79f8c06fa5f025bc4832c719372f25
Tarlan Huseynov <thuseynov@vmi2066266.contaboserver.net>
2024-08-21 18:37:36 +0200

    init

--- modules/eks-resources/main.tf
+++ modules/eks-resources/main.tf
@@ -1,5 +1,6 @@
 /*
-TODO: Where to put awsecrets
+TODO: eks-apps module (Where to put awsecrets - potentially in eks-cluster module?)
+TODO: eks-reloader module
 TODO: ArgoCD notifications & webhook & gitops repo with helms
 TODO: Implement imitation of dynamic Enviironments
 TODO: Lockdown k8s api authentication
