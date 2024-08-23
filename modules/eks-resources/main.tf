4a538a3b59d6f3edb656597fc716293fc52efc58
Tarlan Huseynov <thuseynov@vmi2066266.contaboserver.net>
2024-08-23 12:48:08 +0200

    fus init

--- modules/eks-resources/main.tf
+++ modules/eks-resources/main.tf
@@ -1,4 +1,6 @@
 /*
+TODO: GITHUB ACTIONS RUNNER on k8s
+TODO: argo rollouts optional installation
 TODO: eks-apps module (Where to put awsecrets - potentially in eks-cluster module?)
 TODO: ArgoCD notifications & webhook & gitops repo with helms
 TODO: Implement imitation of dynamic Enviironments
