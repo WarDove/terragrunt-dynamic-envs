6441a6d600716a3ee6216d1b5b18ed260a3191aa
Tarlan Huseynov <thuseynov@vmi2066266.contaboserver.net>
2024-08-29 08:53:11 +0200

    init

--- modules/eks-apps/main.tf
+++ modules/eks-apps/main.tf
@@ -1,6 +1,30 @@
-module "eks-app-permissions" {
+module "eks_app_permissions" {
   source            = "./modules/eks-app-permissions"
   namespace         = var.namespace
   oidc_provider_arn = var.oidc_provider_arn
   app_statements    = {}
+}
+
+locals {
+  eks_app_buckets = [
+    {
+      store-catalog = {
+        versioning = true
+        object_ownership = "BucketOwnerPreferred"
+
+      }
+    }
+
+  ]
+}
+
+module "eks_app_buckets" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 4.1.2"
+
+  bucket                   = "${var.cluster_name}-flow-logs"
+  acl                      = "private"
+  control_object_ownership = true
+  force_destroy            = true
+  object_ownership         = "ObjectWriter"
 }
